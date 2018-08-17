whydu/android_vendor_extras

Create manifest entries from common repos
With this change we will be able to read omni.dependencies from common repositories.

Change-Id: Ic66ceead3601263df0c0e181a45b9ef7e7a32b77

tools/roomservice.py
@@ -267,6 +267,35 @@ def create_dependency_manifest(dependencies):
         os.system("repo sync -f --no-clone-bundle %s" % " ".join(projects))
 
 
+def create_common_dependencies_manifest(dependencies):
+    dep_file = "omni.dependencies"
+    common_list = []
+    if dependencies is not None:
+        for dependency in dependencies:
+            try:
+                index = common_list.index(dependency['target_path'])
+            except ValueError:
+                index = None
+            if index is None:
+                common_list.append(dependency['target_path'])
+                dep_location = '/'.join([dependency['target_path'], dep_file])
+                if not os.path.isfile(dep_location):
+                    sys.exit()
+                else:
+                    try:
+                        with open(dep_location, 'r') as f:
+                            common_deps = json.loads(f.read())
+                    except ValueError:
+                        raise Exception("ERROR: malformed dependency file")
+
+                    if common_deps is not None:
+                        print("Looking for dependencies on: ",
+                               dependency['target_path'])
+                        check_manifest_problems(common_deps)
+                        create_dependency_manifest(common_deps)
+                        create_common_dependencies_manifest(common_deps)
+
+
 def fetch_dependencies(device):
     location = parse_device_from_folder(device)
     if location is None or not os.path.isdir(location):
@@ -275,6 +304,7 @@ def fetch_dependencies(device):
     dependencies = parse_dependency_file(location)
     check_manifest_problems(dependencies)
     create_dependency_manifest(dependencies)
+    create_common_dependencies_manifest(dependencies)
     fetch_device(device)
 
 def check_device_exists(device):

ScrewdStaging/vendor_extras

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

build/tools/roomservice.py
@@ -267,6 +267,35 @@ def create_dependency_manifest(dependencies):
         os.system("repo sync -f --no-clone-bundle %s" % " ".join(projects))
 
 
+def create_common_dependencies_manifest(dependencies):
+    dep_file = "du.dependencies"
+    common_list = []
+    if dependencies is not None:
+        for dependency in dependencies:
+            try:
+                index = common_list.index(dependency['target_path'])
+            except ValueError:
+                index = None
+            if index is None:
+                common_list.append(dependency['target_path'])
+                dep_location = '/'.join([dependency['target_path'], dep_file])
+                if not os.path.isfile(dep_location):
+                    sys.exit()
+                else:
+                    try:
+                        with open(dep_location, 'r') as f:
+                            common_deps = json.loads(f.read())
+                    except ValueError:
+                        raise Exception("ERROR: malformed dependency file")
+
+                    if common_deps is not None:
+                        print("Looking for dependencies on: ",
+                               dependency['target_path'])
+                        check_manifest_problems(common_deps)
+                        create_dependency_manifest(common_deps)
+                        create_common_dependencies_manifest(common_deps)
+
+
 def fetch_dependencies(device):
     location = parse_device_from_folder(device)
     if location is None or not os.path.isdir(location):
@@ -275,6 +304,7 @@ def fetch_dependencies(device):
     dependencies = parse_dependency_file(location)
     check_manifest_problems(dependencies)
     create_dependency_manifest(dependencies)
+    create_common_dependencies_manifest(dependencies)
     fetch_device(device)
 
 def check_device_exists(device):
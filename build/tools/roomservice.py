whydu/android_vendor_extras

Change default remote and branch
Change-Id: I95e054bd39a3c38ab5e656d0a3c06c4015aa8a15

tools/roomservice.py
@@ -34,9 +34,9 @@ except ImportError:
 
 # Config
 # set this to the default remote to use in repo
-default_rem = "github"
+default_rem = "du"
 # set this to the default revision to use (branch/tag name)
-default_rev = "android-4.3"
+default_rev = "n"
 # set this to the remote that you use for projects from your team repos
 # example fetch="https://github.com/dirtyunicorns"
 default_team_rem = "du"

ScrewdStaging/vendor_extras

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

build/tools/roomservice.py
@@ -34,9 +34,9 @@ except ImportError:
 
 # Config
 # set this to the default remote to use in repo
-default_rem = "github"
+default_rem = "du"
 # set this to the default revision to use (branch/tag name)
-default_rev = "android-4.3"
+default_rev = "p9x"
 # set this to the remote that you use for projects from your team repos
 # example fetch="https://github.com/dirtyunicorns"
 default_team_rem = "du"